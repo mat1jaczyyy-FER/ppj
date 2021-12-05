ee3e3acd2cd0cb1df22323fc97887140ec3c55b1
mat1jaczyyy <mat1jaczyyy@gmail.com>
2021-12-05 02:48:05 +0100

    compare: Windows Git Bash support

--- lab1/compare.sh
+++ lab1/compare.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+if uname -o | grep -q "Msys"; then
+	python3="py"
+else
+	python3="python3"
+fi
+
 for i in test/*/ ;
 do
 	echo "Testing $i"
-	python3 LeksickiAnalizator.py < $i/test.in | diff --strip-trailing-cr $i/test.out -
+	
+	if $python3 LeksickiAnalizator.py < $i/test.in | diff --strip-trailing-cr $i/test.out -; then
+		echo "OK"
+	fi
 done
 
